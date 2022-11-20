14c32f985f28edf922041b61abbac09c3f56607a
Ahmad jz <ahmadjzz99@gmail.com>
2022-11-20 14:35:38 +0300

    create register and login views

---
+++ lib/views/login_view.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+              decoration: InputDecoration(hintText: "Enter Email"),
+              controller: _emailTextEditingController,
+              keyboardType: TextInputType.emailAddress,
+              enableSuggestions: false),
+          TextField(
+            decoration: InputDecoration(hintText: "Enter Passord"),
+            controller: _passwordTextEditingController,
+            obscureText: true,
+            enableSuggestions: false,
+          ),
+          TextButton(
+              onPressed: () async {
+                try {
+                  final email = _emailTextEditingController.text;
+                  final password = _passwordTextEditingController.text;
+                  print(email);
+                  print(password);
+                  final cred = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: email, password: password);
+                  print(cred);
+                } on FirebaseAuthException catch (e) {
+                  print(e);
+                }
+              },
+              child: Text("Login"))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/register_view.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reister"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+              decoration: InputDecoration(hintText: "Enter Email"),
+              controller: _emailTextEditingController,
+              keyboardType: TextInputType.emailAddress,
+              enableSuggestions: false),
+          TextField(
+            decoration: InputDecoration(hintText: "Enter Passord"),
+            controller: _passwordTextEditingController,
+            obscureText: true,
+            enableSuggestions: false,
+          ),
+          TextButton(
+              onPressed: () async {
+                final email = _emailTextEditingController.text;
+                final password = _passwordTextEditingController.text;
+                print(email);
+                print(password);
+                try {
+                  final cred = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: email, password: password);
+                  print(cred);
+                } on FirebaseAuthException catch (e) {}
+              },
+              child: Text("Register"))
+        ],
+      ),
+    );
+  }
+}
