9c1be94b34ed0109c0f33bb33d2e61c541327353
Ahmad jz <ahmadjzz99@gmail.com>
2022-11-24 10:37:46 +0300

    add auth services

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:flutter/foundation.dart';
+
+@immutable
+class AuthUser {
+  const AuthUser({required this.isEmailVerified});
+  final bool isEmailVerified;
+
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
+}
